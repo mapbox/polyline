7c7ef4ddd183511d5fa21c84d16006bd431bad93
Michele Belluco <bellucomichele@gmail.com>
2015-02-24 11:44:23 +0100

    New methods for compatibility LatLng object

--- API.md
+++ API.md
@@ -8,3 +8,16 @@ precision defaults to 5.
 
 Takes an array of lat, lon arrays and returns an encoded
 string. If not specified, precision defaults to 5.
+
+
+### polyline.decodeToLatLng(string[, precision])
+
+Takes a string representation of 1+ coordinate pairs 
+and returns an array of LatLng object ((Google Maps Object)[https://developers.google.com/maps/documentation/javascript/reference#LatLng]) . If not specified, 
+precision defaults to 5.
+
+### polyline.encodeFromLatLng(array[, precision])
+
+Takes an array of  LatLng object ((Google Maps Object)[https://developers.google.com/maps/documentation/javascript/reference#LatLng]) and returns an encoded
+string. If not specified, precision defaults to 5.
+
--- test/polyline.test.js
+++ test/polyline.test.js
@@ -1,11 +1,17 @@
 var assert = require('assert'),
     polyline = require('../');
 
+
 describe('polyline', function() {
     var example = [[38.5, -120.2], [40.7, -120.95], [43.252, -126.453]];
+    var exampleLatLng = [];
+
+    for (var i = 0; i < example.length; i++) {
+        exampleLatLng.push( new polyline.LatLng(example[i][0], example[i][1]) );
+    };
 
     describe('#decode()', function() {
-        it('decodes an empty Array', function() {
+        it('decodes an empty Array', function() {  
             assert.deepEqual(polyline.decode(''), []);
         });
 
@@ -16,6 +22,11 @@ describe('polyline', function() {
         it('decodes with a custom precision', function() {
             assert.deepEqual(polyline.decode('_izlhA~rlgdF_{geC~ywl@_kwzCn`{nI', 6), example);
         });
+
+        it('decodes a String into an Array of of object LatLng', function() {
+            assert.deepEqual(polyline.decodeToLatLng('_p~iF~ps|U_ulLnnqC_mqNvxq`@').toString(), exampleLatLng.toString() );
+        });
+
     });
 
     describe('#encode()', function() {
@@ -30,5 +41,10 @@ describe('polyline', function() {
         it('encodes with a custom precision', function() {
             assert.equal(polyline.encode(example, 6), '_izlhA~rlgdF_{geC~ywl@_kwzCn`{nI');
         });
+
+        it('encodes an Object of LatLng into a String', function() {
+            assert.equal(polyline.encodeFromLatLng(exampleLatLng), '_p~iF~ps|U_ulLnnqC_mqNvxq`@');
+        });
+
     });
 });

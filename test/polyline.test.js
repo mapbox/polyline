124462a9ff4312f4d005f0efcba09182c4bb7404
Lyzi Diamond <lyzidiamond@gmail.com>
2015-05-30 16:54:41 -0700

    Add tests

--- test/polyline.test.js
+++ test/polyline.test.js
@@ -1,10 +1,32 @@
 var assert = require('assert'),
-    polyline = require('../');
+    polyline = require('../'),
+    package = require('../package.json');
 
 describe('polyline', function() {
     var example = [[38.5, -120.2], [40.7, -120.95], [43.252, -126.453]],
         // encoded value will enclude slashes -> tests escaping
-        example_slashes = [[35.6,-82.55], [35.59985, -82.55015], [35.6,-82.55]];
+        example_slashes = [[35.6,-82.55], [35.59985, -82.55015], [35.6,-82.55]],
+        example_flipped = [[-120.2, 38.5], [-120.95, 40.7], [-126.453, 43.252]],
+        dependencies = { "turf-flip": "*", "geojson-coords": "*" }
+        geojson = {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "Point",
+                "coordinates": [20.566406, 43.421008]
+            }
+        },
+        flipped_coords = [43.421008, 20.566406];
+
+    describe('#dependencies', function() {
+        it('loads turf-flip as a dependency', function() {
+            assert(package.dependencies['turf-flip']);
+        });
+
+        it ('loads geojson-coords as a dependency', function() {
+            assert(package.dependencies['geojson-coords']);
+        })
+    });
 
     describe('#decode()', function() {
         it('decodes an empty Array', function() {
@@ -42,5 +64,15 @@ describe('polyline', function() {
         it('encodes with a custom precision', function() {
             assert.equal(polyline.encode(example, 6), '_izlhA~rlgdF_{geC~ywl@_kwzCn`{nI');
         });
+    });
+
+    describe('#flip()', function() {
+        it('returns coordinates from geojson', function() {
+            assert.equal(typeof(polyline.flip(geojson)), 'object');
+        });
+
+        it ('flips geometry', function() {
+            assert.deepEqual(polyline.flip(geojson), [flipped_coords]);
+        })
     });
 });

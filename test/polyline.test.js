ca7e667a1cac6f7234c44a362827665873953bd4
Lyzi Diamond <lyzidiamond@gmail.com>
2015-06-19 16:46:53 -0700

    update tests

--- test/polyline.test.js
+++ test/polyline.test.js
@@ -1,32 +1,18 @@
 var assert = require('assert'),
-    polyline = require('../'),
-    package = require('../package.json');
+    polyline = require('../');
 
 describe('polyline', function() {
     var example = [[38.5, -120.2], [40.7, -120.95], [43.252, -126.453]],
         // encoded value will enclude slashes -> tests escaping
         example_slashes = [[35.6,-82.55], [35.59985, -82.55015], [35.6,-82.55]],
-        example_flipped = [[-120.2, 38.5], [-120.95, 40.7], [-126.453, 43.252]],
-        dependencies = { "turf-flip": "*", "geojson-coords": "*" }
-        geojson = {
-            "type": "Feature",
-            "properties": {},
+        example_flipped = [[-120.2, 38.5], [-120.95, 40.7], [-126.453, 43.252]];
+        geojson = { "type": "Feature",
             "geometry": {
-                "type": "Point",
-                "coordinates": [20.566406, 43.421008]
-            }
-        },
-        flipped_coords = [43.421008, 20.566406];
-
-    describe('#dependencies', function() {
-        it('loads turf-flip as a dependency', function() {
-            assert(package.dependencies['turf-flip']);
-        });
-
-        it ('loads geojson-coords as a dependency', function() {
-            assert(package.dependencies['geojson-coords']);
-        })
-    });
+                "type": "LineString",
+                "coordinates": example_flipped
+            },
+            "properties": {}
+        }
 
     describe('#decode()', function() {
         it('decodes an empty Array', function() {
@@ -66,13 +52,16 @@ describe('polyline', function() {
         });
     });
 
-    describe('#flip()', function() {
-        it('returns coordinates from geojson', function() {
-            assert.equal(typeof(polyline.flip(geojson)), 'object');
+    describe('#fromGeoJSON()', function() {
+        it('throws for non linestrings', function() {
+            assert.throws(function() {
+                polyline.fromGeoJSON({});
+            }, /Input must be a GeoJSON LineString/);
         });
 
-        it ('flips geometry', function() {
-            assert.deepEqual(polyline.flip(geojson), [flipped_coords]);
-        })
+        it('flips coordinates and encodes', function() {
+            assert.equal(polyline.fromGeoJSON(geojson), '_p~iF~ps|U_ulLnnqC_mqNvxq`@');
+        });
     });
+
 });

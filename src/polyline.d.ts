1d0f5adcc1c84675ad0e8525e1b4eb6caea4cc1f
Brad Martin <bradwaynemartin@gmail.com>
2017-10-05 12:57:34 -0500

    add TS def & remove .js extension on main for module resolution

---
+++ src/polyline.d.ts
@@ -0,0 +1,39 @@
+/**
+ * Encodes the given [latitude, longitude] coordinates array.
+ *
+ * @param {Array.<Array.<Number>>} coordinates
+ * @param {Number} precision
+ * @returns {String}
+ */
+export function encode(coordinates, precision): string;
+
+/**
+ * Decodes to a [latitude, longitude] coordinates array.
+ *
+ * This is adapted from the implementation in Project-OSRM.
+ *
+ * @param {String} str
+ * @param {Number} precision
+ * @returns {Array}
+ *
+ * @see https://github.com/Project-OSRM/osrm-frontend/blob/master/WebContent/routing/OSRM.RoutingGeometry.js
+ */
+export function decode(str: string, precision: number): any[];
+
+/**
+ * Encodes a GeoJSON LineString feature/geometry.
+ *
+ * @param {Object} geojson
+ * @param {Number} precision
+ * @returns {String}
+ */
+export function fromGeoJSON(geojson: object, precision: number): string;
+
+/**
+ * Decodes to a GeoJSON LineString geometry.
+ *
+ * @param {String} str
+ * @param {Number} precision
+ * @returns {Object}
+ */
+export function toGeoJSON(str: string, precision: number): object;

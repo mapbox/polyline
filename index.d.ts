e2b064ddade6907de7d3b672427872bb16ab92ef
rinzeb <rinzeb@users.noreply.github.com>
2018-08-13 13:45:05 +0200

    Create index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,13 @@
+import {GeoJSON} from 'geojson';
+
+interface Polyline {
+  decode(string: string, precision?: number): number[][];
+  encode(coordinate: number[][], precision?: number): string;
+  fromGeoJSON(geojson: GeoJSON.LineString | GeoJSON.Feature<GeoJSON.LineString>, precision?: number): string;
+}
+
+declare var polyline: Polyline;
+
+declare module "polyline" {
+    export = polyline;
+}
